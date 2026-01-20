f8f44d19b20e70bdf9954f63607b79a4c345b69b
Binato <binato@gmail.com>
2026-01-20 14:48:13 -0300

    alterado logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,10 +10,10 @@ const Header: React.FC<HeaderProps> = ({ onToggleTheme }) => {
     <header className="mb-8 border-b-4 border-black dark:border-white pb-6 flex flex-col md:flex-row items-center justify-between gap-6">
       <div className="flex items-center gap-6">
         <div className="w-16 h-16 bg-white rounded-full brutal-border flex items-center justify-center overflow-hidden brutal-shadow flex-shrink-0">
-          <img 
-            alt="Referencia Logo" 
-            className="w-12 h-12" 
-            src="https://lh3.googleusercontent.com/aida-public/AB6AXuDuqlpJNz2M2sNYMDIsQlVvYy_yW3SvRCKCMYWl0JP-5Hr5abJe0CkysC8RxoL9I-OorUv4JeOe1FyPMV291U-IK8LFytmlRfb-zjUz-ugEflhi5WggevkiantJ8AlQAAxyQ90ioXaO4bOyPJLPWaVOfifNnhKmQADm9BCjgjK6FXxnBaDiC7H_m10oiKN1VrJ2j1EHU-CDec3Ge6rkN-vbETj1LBtGhKB9XVsBFLHHw_yG-UHaw5E38wRGmnXHnNKnoJ1aRTKUNyWT" 
+          <img
+            alt="Referencia Logo"
+            className="w-12 h-12 object-contain"
+            src="/logo.png"
           />
         </div>
         <div>
@@ -21,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({ onToggleTheme }) => {
           <p className="text-lg font-bold text-primary uppercase">Referencia - Monitoramento dos Agentes</p>
         </div>
       </div>
-      <button 
+      <button
         className="btn-brutal bg-black hover:bg-gray-800 dark:bg-white dark:hover:bg-gray-200 dark:text-black text-white flex items-center justify-center gap-2 py-2 px-6 font-black"
         onClick={onToggleTheme}
       >
